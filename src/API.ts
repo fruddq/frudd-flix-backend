7d69fea1336fdd4bad115bb5de25d34beef49e1f
Frudd <giadi735@gmail.com>
2023-02-12 12:15:22 +0100

    WIP

--- src/API.ts
+++ src/API.ts
@@ -1,27 +1,56 @@
 import axios from 'axios'
-import { API_URL, API_CONFIG, requestDiscover } from './services/config.js'
+import type { IAPIResponse, IMovie } from './models/Models.js'
+import { API_URL } from './services/config.js'
+import dotenv from 'dotenv'
 
 export class API {
   constructor() {
-    // @TODO Change this information depending on test or production
+    dotenv.config()
   }
 
-  // @TODO ensure ENV functions as expcted
-  // const config = { params: { api_key: process.env["API_KEY"] } }
+  async fetchData() {
+    const API_CONFIG = { params: { api_key: process.env['API_KEY'] } }
 
-  async fetchData(request: string) {
     try {
-      const {
-        data: { results },
-      } = await axios.get(`${API_URL}/${request}`, API_CONFIG)
+      const response: IAPIResponse = await axios.get(API_URL, API_CONFIG)
+      const results = response.data.results as IMovie[]
       return results
     } catch (err) {
       console.error(err)
+      return []
     }
   }
 }
 
+// const API_URL2 =
+//   'https://api.themoviedb.org/3/discover/movie?api_key=edfb4a11c2c5f2ff5f3e1ef08db80649&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=true&page=1&primary_release_date.gte=2021-01-01&primary_release_date.lte=2022-12-31&with_genres=35,18'
+// // const API_URL =
 // const api = new API()
 
-// const test = await api.fetchData(requestDiscover)
+// const test = await api.fetchData()
 // console.log(test)
+
+// const test = await axios.get(API_URL2)
+// console.log(test.data.results)
+
+// const API_URL = 'https://api.themoviedb.org/3/discover/movie'
+
+// const API_CONFIG = {
+// params: {
+// api_key: process.env['API_KEY'] ,
+// language: 'en-US',
+// sort_by: 'popularity.desc',
+// include_adult: false,
+// include_video: false,
+// page: 1,
+// primary_release_date: {
+// gte: '2021-01-01',
+// lte: '2022-12-31',
+// },
+// with_genres: '35,18',
+// },
+// }
+
+// Then you can make the API request like this:
+
+// const response: IAPIResponse = await axios.get(API_URL, API_CONFIG)
--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,17 +1,38 @@
+import axios from 'axios'
 import express from 'express'
 import { API } from '../API.js'
-import { requestDiscover } from '../services/config.js'
 
 export const router = express.Router()
 
 const api = new API()
 
 router.get('/home', async (_req, res) => {
-  const results = await api.fetchData(requestDiscover)
+  const results = await api.fetchData()
   res.send(results)
 })
 
-// router.get('/test', async (_req, res) => {
-//   const results = await api.fetchData(requestDiscover)
-//   res.send(results)
-// })
+router.get('/browse', (req, res) => {
+  const from = Number(req.query['from'])
+  const to = Number(req.query['to'])
+  const genres = req.query['genres']
+  console.log(genres)
+  res.send(`${from}, ${to}, and ${genres} `)
+})
+
+export const fetchFrontEnd = async (from: number, to: number, genres: string) => {
+  try {
+    const response = await axios.get('http://localhost:3000/browse', {
+      params: {
+        from,
+        to,
+        genres,
+      },
+    })
+    return response.data
+  } catch (error) {
+    console.error(error)
+  }
+}
+;(async () => {
+  console.log(await fetchFrontEnd(2022, 2023, '1,2,3'))
+})()
